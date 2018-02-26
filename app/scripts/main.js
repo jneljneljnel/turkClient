a0a0314b647c2df1635a6ac2f121a5b919106bbb
Jeremy Nelson <jnelson@dm.ai>
2018-02-26 15:41:20 -0800

    bucket fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,19 @@
+let folder = 'laugh';
+let question = 'Record a laugh.';
+
+
 let recordButton, stopButton, recorder, submitButton, filename, word;
 let blob = [];
 let url = {
-  heroku : 'https://dmaiaudio.herokuapp.com/api/kb8',
-  dev : 'http://localhost:3300/api/kb8'
+  heroku : 'https://dmaiaudio.herokuapp.com/api/',
+  dev : 'http://localhost:3300/api/'
 }
 let options = {mimeType: 'audio/ogg'};
 let random = Math.floor((Math.random() * 2) + 1);
 let randomFile = Math.floor((Math.random() * 3) + 1);
 let posting = false;
 
+let random5 = Math.floor(Math.random()*90000) + 10000;
 
 if (randomFile == 1){
   word = 'Ball';
@@ -31,7 +36,7 @@ if (!isSecureOrigin) {
     '\n\nChanging protocol to HTTPS');
   location.protocol = 'HTTPS';
 }
-
+document.getElementById('question').innerHTML = question
 document.getElementById('image').src = filename;
 document.getElementById('secondImage').src = filename;
 document.getElementById('singular').innerHTML = word
@@ -117,16 +122,17 @@ function post(){
   let tag = document.getElementById('tag').value
   let formData = new FormData()
   let datenow = Date.now().toString()
-   formData.append( randomFile+'_'+random+'_'+datenow, blob[0]);
-   formData.append('filename', randomFile+'_'+random+'_'+datenow);
-   formData.append('plural', random);
-   formData.append('tag', tag);
-   formData.append('fileId', randomFile);
+  formData.append('filename', randomFile+'_'+random+'_'+datenow);
+  formData.append('plural', random);
+  formData.append('tag', tag);
+  formData.append('folder', folder);
+  formData.append('fileId', randomFile);
+  formData.append( randomFile+'_'+random+'_'+datenow, blob[0]);
    console.log('POST', blob[0], blob[0].size);
    if(posting == false) {
      posting = true;
       $.ajax({
-        url: 'https://dmaiaudio.herokuapp.com/api/dmailaugh',
+        url: url.heroku+'dmai'+folder,
         type: 'POST',
         data:formData,
         processData: false,
@@ -134,7 +140,7 @@ function post(){
         success: function(data) {
                 console.log(data);
                 posting = false;
-                alert('Survey code is: 55555');
+                alert('Survey code is: '+random5);
 
         }
       });
